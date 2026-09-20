7542f766fdcbbf15a49aa7f794232b7655e5caf1
achintk27 <ak13506@nyu.edu>
2026-09-20 17:40:25 -0400

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.py
@@ -0,0 +1,56 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        #sort the array in accending order 
+        nums.sort()
+
+        result = []
+
+        #enum and store nums and index 
+        for index , number in enumerate(nums) :
+            
+            #since its in inc order , if the number is not -ve or 0 and is > 0 the other 2 nums will also be >0 and cant give the triplet
+            if number > 0:
+                break
+
+            #skip duplicate nums first , so we dont get same triplets 
+            #if number is = to previous number , skip 
+            if index > 0 and number == nums[index - 1]:
+                continue
+                 
+
+            # we begin left with immediate next number compared to what we are traversing
+            left = index + 1
+
+            #right is the last number 
+            right = len(nums) - 1
+
+            while left < right : 
+                current_sum = number + nums[left] + nums[right]
+
+                if current_sum < 0:
+
+                    #since it is in increasing order left needs to be incremeneted  
+                    left +=1 
+                
+                elif current_sum > 0 :
+                    right -=1
+
+                #append the triplet onto the result array 
+                else :
+                    result.append([number ,nums[left],nums[right]])
+
+                
+                    left+= 1
+                    right -=1
+                
+                    #skip duplicate left values :
+                    while left < right and nums[left] == nums[left - 1] :
+                        left+=1
+                
+        return result
+
+            
+
+
+
